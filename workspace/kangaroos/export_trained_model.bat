a0b93c5f28f9355a19afbec992ac42a945cabd9e
Letlhogonolo Segoe <noloxtreme@gmail.com>
2020-06-07 18:43:37 +0200

    Consolidated road damage datasets

--- workspace/kangaroos/export_trained_model.bat
+++ workspace/kangaroos/export_trained_model.bat
@@ -1,8 +1,8 @@
 @echo off
 set ORIGINALPATH=%PYTHONPATH%;
 set PYTHONPATH=%PYTHONPATH%;research\slim
-set TRAINED_CHECKPOINT=D:/university/datasets/Kangaroo/kangaroo/training/model.ckpt-500
+set TRAINED_CHECKPOINT=D:/university/datasets/Kangaroo/kangaroo/training/model.ckpt-100
 set OUTPUT_DIRECTORY=D:/university/datasets/Kangaroo/kangaroo/training/output
-python research\object_detection\export_inference_graph.py --input_type image_tensor --pipeline_config_path=workspace/kangaroos/pipeline/ssd_mobilenet_v1_coco_v1.config --trained_checkpoint_prefix=%TRAINED_CHECKPOINT% --output_directory=%OUTPUT_DIRECTORY%
+python research\object_detection\export_inference_graph.py --input_type image_tensor --pipeline_config_path=workspace/kangaroos/pipeline/ssd_mobilenet_v1_coco_v1_1_class.config --trained_checkpoint_prefix=%TRAINED_CHECKPOINT% --output_directory=%OUTPUT_DIRECTORY%
 set PYTHONPATH=%ORIGINALPATH%
 @echo on
